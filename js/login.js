69a98728a3ac70e1107a5fb6a3c7218b191e5fcb
Alex Felix <85413825+afsilva3021@users.noreply.github.com>
2022-11-27 23:26:03 -0300

    cadastro
    castro e home

--- js/login.js
+++ js/login.js
@@ -1,3 +1,14 @@
+function Cad() {
+  var cadastro = document.getElementsByName("useremail").value.toLowerCase();
+  var senha = document.getElementsByName("senha").value;
+  var novoUsuario ={
+    "login" : "abc\@teste.org",
+    "senha":"xxxx"
+    }
+    
+    usuarios.push(novoUsuario)
+}
+
 var usuarios = [
     {"email": "afsilva3021@uni9.edu.br", "senha": "123"},
     {"email": "leo10machadosiqueira@uni9.edu.br", "senha": "123"}
@@ -6,16 +17,15 @@ var usuarios = [
   function Login() {
     var usuario = document.getElementsByName('email')[0].value.toLowerCase();
     var senha = document.getElementsByName('password')[0].value;
-
     for (var u in usuarios) {
         var us = usuarios[u];
         if (us.email == usuario && us.senha == senha) {
             window.location = "musica.html";
             return true;
         }
+
     }
     alert("Dados incorretos, tente novamente.");
     return false;
   }
 
-  console.log(alert);
