5ed2d5e58c60ac701a2a0bdb98f6833834fa20b0
Alex Felix <85413825+afsilva3021@users.noreply.github.com>
2022-11-30 10:07:48 -0300

    login
    atualizado

--- js/login.js
+++ js/login.js
@@ -1,17 +1,8 @@
 var usuarios = [
     {"email": "afsilva3021@uni9.edu.br", "senha": "123"},
     {"email": "leo10machadosiqueira@uni9.edu.br", "senha": "123"},
-    
+    {"email": "professor", "senha": "123"},
 ];
-
-
-  function add(){
-    for(var e in usuario)
-    const NovoUsuario = document.getElementsByName("userEmail")[3,4,5].Valeu.toLowerCase();
-    const NovaSenha = document.getElementsByName("senha")[3,4,5].value;
-    for (let s in usuarios)  
-  
-  }
   
   function Login() {
     var usuario = document.getElementsByName('email')[0].value.toLowerCase();
